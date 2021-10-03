c083883279a5db8c1224598a6ba54e7f1bac8fc5
prof_meo <34885297+shhusband0810@users.noreply.github.com>
2021-10-03 23:57:27 +0900

    Update Team.py

--- Team.py
+++ Team.py
@@ -12,53 +12,43 @@
 # 6. 한 팀의 챔피언이 모두 사망할때까지 반복한다.
 # 7. 게임이 종료되면 이긴 팀을 출력하자.
 # 8. 완성된 코드는 github에 업로드할 것.
-
-import random
-import requests
-import json
-from Team import *
 from Champion import *
 
-class Game:
-    #게임 생성자
-    def __init__(self):
-        champion_info = json.loads(requests.get("http://ddragon.leagueoflegends.com/cdn/11.17.1/data/ko_KR/champion.json").text)["data"]
-        # 무작위로 챔피언 10개 생성 및 5개씩 redteam, blueteam 생성
-        data = random.sample(list(champion_info), 10)
-        self.redteam = Team(data[:5])
-        self.blueteam = Team(data[5:])
-
-    # 게임 시작
-    def game_start(self):
-        print("게임이 시작되었습니다.")
-        while self.redteam.are_team_all_dead() or self.blueteam.are_team_all_dead():
-            for champion in self.set_attack_sq(self.redteam.get_member_name(), self.blueteamget_member_name()):
-                if champion in self.redteam.get_member_name():
-                    foe = random.choice(self.blueteam.get_member())
-                    foe.set_hp(champion.attack(foe.get_name()))
-                else:
-                    foe = random.choice(self.redteam.get_member())
-                    foe.set_hp(champion.attack(foe.get_name()))
-        if self.redteam.are_team_all_dead() == True:
-            print("게임이 종료 되었습니다. blueteam이 승리하였습니다.")
+class Team:
+#팀 생성자 생성
+    def __init__(self, member):
+        self.team = []
+        if len(member) == 5:
+            for champ in member:
+                self.team.append(Champion(champ))
         else:
-            print("게임이 종료 되었습니다. redteam이 승리하였습니다.")
+            Print("팀 내의 챔피언의 수가 5명이어야 합니다.")
+        self.team_all_dead = False
 
+#Team 클래스 getter
+    def get_member(self):
+        return self.team
 
-    # 공격 순서 정의
-    def set_attack_sq(self, team1, team2):
-        all_champion = team1 + team2
-        champion_and_speed = []
-        attack_order = []
-        for champion in all_champion:
-            champion_and_speed.append([champion, Champion(champion).get_movespeed()])
-        sorted(champion_and_speed, key=lambda x: x[1], reverse=True)
-        #       sorted(champion_and_speed, key=lambda x: -x[1])
-        i = 0
-        while i < len(champion_and_speed):
-            attack_order.append(champion_and_speed[i][0])
-            i += 1
-        print("공격순서는 {attack_order}입니다.")
-        return attack_order
+    def get_member_name(self):
+        member_name = []
+        for champion in self.team:
+            member_name.append(champion.get_name())
+        return member_name
 
+    def get_member_hp(self):
+        member_hp = []
+        for champion in self.team:
+            member_hp.append([champion.get_name(), champion.get_hp()])
+        return member_hp
 
+    def get_member_movespeed(self):
+        member_movespeed = []
+        for champion in self.team:
+            member_hp.append([champion.get_name(), champion.get_movespeed()])
+        return member_hp
+
+    def are_team_all_dead(self):
+        for champion in self.get_member():
+            if champion.get_is_dead() == True:
+                return
+        return False
